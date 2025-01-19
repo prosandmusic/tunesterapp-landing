fad47669ae32401dfc6286fac154aecb76f9b8e6
unknown <scottbeeker@create-react-app.org>
2025-01-18 23:26:06 -0800

    react 19 scaffold
    -- Scott Beeker 1/18/25 11:25 PM PST

--- README.md
+++
@@ -1,2 +0,0 @@
-# tunesterapp-landing
-landingpage for tunesterapp #tunester2025 #tunesterapp #tunesterlanding #musicstreaming #ipfs #ppv #liveevents #entertainment
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
