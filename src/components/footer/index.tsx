dbd7846e62d75a4e70928b680327681192e0f079
scott <bayareadevlead@gmail.com>
2025-04-14 18:43:57 -0700

    aritst links added to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,34 +1,56 @@
 import React from "react";
 import { BsXCircleFill, BsInstagram, BsFillThreadsFill, BsFacebook } from "react-icons/bs";
-import * as stylex from "@stylexjs/stylex"; 
+import * as stylex from "@stylexjs/stylex";
 
-const footerstyles = stylex.create({
+const footerStyles = stylex.create({
   footer: {
-    padding: '5rem',
-    background: '#222',
-    color: 'white',
-    display: 'grid',
-    gridTemplateColumns: '1fr 2fr 1fr',
-    gridTemplateRows: 'auto auto', // Add a second row for socials
-    alignItems: 'center',
-    gap: '1rem',
+    padding: "5rem",
+    background: "#222",
+    color: "white",
+    display: "grid",
+    gridTemplateColumns: "1fr 2fr 1fr",
+    gridTemplateRows: "auto auto",
+    alignItems: "center",
+    gap: "1rem",
   },
   socials: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, auto)', // Adjusted for icon sizing
-    gridColumn: '3', // Move to the third column (right)
-    gridRow: '2', // Move to the second row (bottom)
-    justifyContent: 'end', // Align icons to the right
-    alignItems: 'end', // Align icons to the bottom
-    gap: '1rem', // Space between icons
+    display: "grid",
+    gridTemplateColumns: "repeat(4, auto)",
+    gridColumn: "3",
+    gridRow: "2",
+    justifyContent: "end",
+    alignItems: "end",
+    gap: "1rem",
   },
   artists: {
-    alignItems: 'baseline',
+    gridColumn: "1",
+    gridRow: "1",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
   },
   copyright: {
-    gridColumn: '3',
-    gridRow: '1', // Keep in the first row
-    textAlign: 'right',
+    gridColumn: "3",
+    gridRow: "1",
+    textAlign: "right",
+  },
+  navLinks: {
+    gridColumn: "2",
+    gridRow: "1",
+    display: "flex",
+    justifyContent: "center",
+    gap: "1rem",
+  },
+  artistLink: {
+    color: "white",
+    textDecoration: "none",
+    fontSize: "16px",
+    marginBottom: "0.5rem",
+  },
+  socialIcon: {
+    color: "white",
+    fontSize: "24px",
+    textDecoration: "none",
   },
 });
 
@@ -36,52 +58,71 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   const socialLinks = [
-    { icon: <BsInstagram />, url: 'https://instagram.com/tunestermusic' },
-    { icon: <BsFillThreadsFill />, url: 'https://threads.net/tunestermusic' },
-    { icon: <BsXCircleFill />, url: 'https://x.com/tunestermusic' },
-    { icon: <BsFacebook />, url: 'https://facebook.com/tunestermusic' },
-    ];
+    { icon: <BsInstagram />, url: "https://instagram.com/tunestermusic" },
+    { icon: <BsFillThreadsFill />, url: "https://threads.net/tunestermusic" },
+    { icon: <BsXCircleFill />, url: "https://x.com/tunestermusic" },
+    { icon: <BsFacebook />, url: "https://facebook.com/tunestermusic" },
+  ];
 
-  // Sample navLinks (since it wasn't provided)
-  const navLinks = ['Home', 'Features', 'FAQ', 'Investors'];
-  const artistLinks = ['Profiles', 'Groups', 'FAQ', 'Tour Guide', 'Creator Cloud Toolkit'];
+  const navLinks = ["Home", "Features", "FAQ", "Investors"];
+
+  const artistLinks = [
+    { title: "Profiles", url: "https://artists.tunester.online/profiles" },
+    { title: "Groups", url: "https://artists.tunester.online/groups" },
+    { title: "FAQ", url: "https://artists.tunester.online/faq" },
+    { title: "Tour Guide", url: "https://artists.tunester.online/tourguide" },
+    { title: "Creator Cloud Toolkit", url: "https://artists.tunester.online/creatorcloudtoolkit" },
+  ];
 
   return (
-    <footer {...stylex.props(footerstyles.footer)}>
+    <footer {...stylex.props(footerStyles.footer)}>
       {/* Artists Section */}
-      <div>
+      <div {...stylex.props(footerStyles.artists)}>
         <h4 style={{ margin: 0 }}>Artist Resources</h4>
+        {artistLinks.map(({ title, url }, index) => (
+          <a
+            key={index}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            {...stylex.props(footerStyles.artistLink)}
+          >
+            {title}
+          </a>
+        ))}
+      </div>
+
+      {/* Nav Links Section */}
+      <div {...stylex.props(footerStyles.navLinks)}>
+        {navLinks.map((link) => (
+          <a
+            key={link}
+            href={`#${link.toLowerCase()}`}
+            {...stylex.props(footerStyles.artistLink)}
+          >
+            {link}
+          </a>
+        ))}
+      </div>
+
+      {/* Copyright Section */}
+      <div {...stylex.props(footerStyles.copyright)}>
+        <span>Copyright © {currentYear} Tunester</span>
       </div>
 
       {/* Social Links Section */}
-      <div {...stylex.props(footerstyles.socials)}>
+      <div {...stylex.props(footerStyles.socials)}>
         {socialLinks.map(({ icon, url }, index) => (
           <a
             key={index}
             href={url}
             target="_blank"
             rel="noopener noreferrer"
-            style={{ color: 'white', fontSize: '24px', textDecoration: 'none' }}
+            {...stylex.props(footerStyles.socialIcon)}
           >
             {icon}
           </a>
         ))}
-      </div>
-
-      {/* Copyright and Nav Links Section */}
-      <div {...stylex.props(footerstyles.copyright)}>
-        <span>Copyright © {currentYear} Tunester</span>
-        <div style={{ marginTop: '1rem' }}>
-          {navLinks.map((link) => (
-            <a
-              key={link}
-              href={`#${link.toLowerCase()}`}
-              style={{ color: 'white', textDecoration: 'none', marginLeft: '1rem' }}
-            >
-              {link}
-            </a>
-          ))}
-        </div>
       </div>
     </footer>
   );
